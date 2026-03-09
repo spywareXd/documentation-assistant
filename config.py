29a47ecd78032bdf8c530e987c03a85c5d742499
Ashwin <rajashwin1909@gmail.com>
2026-03-10 04:16:45 +0530

    minor changes

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+#Enter User input for subject, URL and scraping instructions
 KNOWLEDGE_BASE_SUBJECT = "Dark Souls Lore"
 KNOWLEDGE_BASE_URL = "https://darksouls.fandom.com/wiki/Lore"
 SCRAPING_INSTRUCTIONS ="Map only actual Dark Souls story lore and character lore. Ignore Everything else"
